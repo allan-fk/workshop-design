4e3c30108b1341524d86fad8c174fc2f570b38dd
klein <allan.klein@epitech.eu>
2022-02-07 11:10:44 +0100

    bryan-work

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,13 +1,18 @@
+import React, { useState } from 'react';
 import { Form, Input, Button, Checkbox, Col, Row, Layout, Typography } from 'antd';
 import {
   GoogleOutlined
 } from '@ant-design/icons';
+import SignIn from './SignIn';
 
-const { Title, Text } = Typography;
+const { Title, Text, Link } = Typography;
 
 const { Sider, Content } = Layout;
 
 const Login = () => {
+  const [width, setWidth] = useState('70%');
+  const [loginIsVisible, setLoginIsVisible] = useState(true);
+
   const onFinish = (values) => {
     console.log('Success:', values);
   };
@@ -18,10 +23,12 @@ const Login = () => {
 
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider width={'50%'} theme={'light'} style={{ zIndex: 2 }}>
-        <Row justify="space-around" align="middle" style={{height: "100vh"}}>
-          <Col span={12}>
-            <Row style={{margin: "40px 0"}}>
+      <Sider width={'50%'} collapsedWidth={'100%'} breakpoint="md" theme={'light'} style={{ zIndex: 2, width: '50%' }
+      }>
+        {loginIsVisible &&
+        <Row justify="space-around" align="middle" style={{ height: "100vh" }}>
+          <Col xl={12} lg={16} md={16} sm={12} xs={20}>
+            <Row style={{ margin: "40px 0" }}>
               <Col span={24}>
                 <Title>Welcome back</Title>
               </Col>
@@ -84,19 +91,24 @@ const Login = () => {
                   span: 25,
                 }}
               >
-                <Button type="primary" htmlType="submit" size='large' style={{width: "100%", margin: "3% 0"}}>
-                  Sign In
+                <Button type="primary" htmlType="submit" size='large' style={{ width: "100%", margin: "3% 0" }}>
+                  Login
                 </Button>
-                <Button htmlType="submit" size='large' style={{width: "100%"}}>
-                <GoogleOutlined /> Sign In with Google
+                <Button htmlType="submit" size='large' style={{ width: "100%" }}>
+                  <GoogleOutlined /> Login with Google
                 </Button>
+                <Row style={{margin:"5px 0"}}>
+                <Text style={{marginRight:"5px"}}>Not register yet?</Text><Link type="link" onClick={() => setLoginIsVisible(false)}> Create an Account</Link>
+                </Row>
               </Form.Item>
             </Form>
           </Col>
         </Row>
+        }
+        {!loginIsVisible && <SignIn/>}
       </Sider>
       <Content span={12} style={{ backgroundColor: 'blue' }}>
-        <img src={"https://i1.wp.com/images.frandroid.com/wp-content/uploads/2021/06/windows-11-wallpaper-fond-ecran-4.jpg?ssl=1"} alt="Logo" style={{ zIndex: 1, position: "relative", left: "-50%", height: "100vh" }} />
+        <img src={"https://i1.wp.com/images.frandroid.com/wp-content/uploads/2021/06/windows-11-wallpaper-fond-ecran-4.jpg?ssl=1"} alt="Logo" style={{ zIndex: 1, position: "fixed", left: "50%", height: "105%", transform: "translate(-25%, 0px)", minHeight: "400px" }} />
       </Content>
     </Layout>
   );
---
+++ src/Login/SignIn.js
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox, Col, Row, Layout, Typography, Select, DatePicker } from 'antd';
+import {
+  GoogleOutlined
+} from '@ant-design/icons';
+
+const { Title, Text } = Typography;
+
+const { Sider, Content } = Layout;
+
+const SignIn = () => {
+  const [width, setWidth] = useState('70%');
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const getPrefixPhones = () => {
+    let prefixPhones = [];
+    for (let i = 0; i < 100; i++) {
+      prefixPhones.push(<Select.Option value={i}>{`+${i}`}</Select.Option>);
+    }
+    return prefixPhones;
+  };
+
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        {getPrefixPhones()}
+      </Select>
+    </Form.Item>
+  );
+
+  return (
+    <Row justify="space-around" align="middle" style={{ height: "100vh" }}>
+      <Col xl={12} lg={16} md={16} sm={12} xs={20}>
+        <Row style={{ margin: "40px 0" }}>
+          <Col span={24}>
+            <Title>Register</Title>
+          </Col>
+        </Row>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 24,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          layout="vertical"
+        >
+
+          <Form.Item
+            label="Firstname"
+            name="firstname"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your firstname !',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Lastname"
+            name="lastname"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your lastname !',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="birthday" label="Birthday" rules={[
+            {
+              type: 'object',
+              required: true,
+              message: 'Please select your birthday !',
+            },
+          ]}>
+            <DatePicker />
+          </Form.Item>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username !',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password !',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="phone"
+            label="Phone Number"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your phone number!',
+              },
+            ]}
+          >
+            <Input
+              addonBefore={prefixSelector}
+              style={{
+                width: '100%',
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email !',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              span: 25,
+            }}
+          >
+            <Button type="primary" htmlType="submit" size='large' style={{ width: "100%", margin: "3% 0" }}>
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignIn
